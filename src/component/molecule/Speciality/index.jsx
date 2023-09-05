868069a7ea64b502c605501a7e2cf5b2fb44b75b
ovara1mark <markovara1@gmail.com>
2023-09-05 21:24:50 +0100

    update

--- src/component/molecule/Speciality/index.jsx
+++ src/component/molecule/Speciality/index.jsx
@@ -4,6 +4,7 @@ import video_hero9 from "../../../assets/Frame9.svg";
 import video_hero10 from "../../../assets/Frame10.svg";
 import video_hero11 from "../../../assets/Frame11.svg";
 import video_hero12 from "../../../assets/Frame12.svg";
+import star_white from "../../../assets/star-white.svg";
 
 export const Speciality = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -50,7 +51,7 @@ export const Speciality = () => {
 
   return (
     <>
-      <section className="lg:pt-[8.25rem] lg:px-[5.31rem] md:pt-[6.8rem] md:px-[2.38rem] px-[1.5rem] pt-[7.97rem] special-section">
+      <section className="lg:pt-[8.25rem] lg:px-[5.31rem] md:pt-[6.8rem] md:px-[2.38rem] px-[1.5rem] pt-[7.97rem] special-section border">
         <div className="flex justify-center">
           <div ref={slideRef} className="transition ease-in-out delay-50">
             <div className="xl:w-[69.375rem] xl:h-[38.125rem] rounded-[0.75rem]">
@@ -60,6 +61,45 @@ export const Speciality = () => {
                 className="object-cover rounded-[0.75rem]"
               />
             </div>
+          </div>
+        </div>
+        <div className="lg:flex lg:justify-end">
+          <div className="lg:pt-[3.25rem] md:pt-[7.33rem] pt-[9rem] lg:w-[45rem] border">
+            <div>
+              <span className="md:text-[2.5rem] text-upsel-light-grey font-bold text-[2rem]">
+                Speciality
+              </span>
+            </div>
+            <div>
+              <div className="pt-[2rem]">
+                <p className="md:text-[1.125rem] text-upsel-light-grey font-normal md:w-[24.5rem] w-[14.3rem] md:leading-[1.625rem] text-[0.75rem] leading-[1.375rem]">
+                  We specialise in crafting tailor-made websites that propel
+                  health and wellness businesses to success.
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <img src={star_white} alt="star" />
+                <span className="md:text-[1.75rem]  text-[1.25rem] font-bold text-upsel-light-grey ">
+                  Health-tech
+                </span>
+              </div>
+            </div>
+            <div>
+              <div className="pt-[2.5rem]">
+                <p className="md:text-[1.125rem] text-upsel-light-grey font-normal md:w-[30.5rem] w-[15.3rem] md:leading-[1.625rem] text-[0.75rem] leading-[1.375rem]">
+                  From seamless online registration to dynamic event schedules,
+                  our expert team collaborates with you to deliver exceptional
+                  online experiences for your clients and events attendees.
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <img src={star_white} alt="star" />
+                <span className="md:text-[1.75rem]  text-[1.25rem] font-bold text-upsel-light-grey ">
+                  Events & Entertainment -tech
+                </span>
+              </div>
+            </div>
+            <div></div>
           </div>
         </div>
       </section>
