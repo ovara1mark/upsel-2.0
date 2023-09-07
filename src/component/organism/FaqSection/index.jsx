0d47ad9c7802b04c13f0bf63d43c971c0601299f
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2023-09-07 12:19:21 +0100

    faq complete

--- src/component/organism/FaqSection/index.jsx
+++ src/component/organism/FaqSection/index.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import search from "../../../assets/Search.svg";
+import { frequent } from "./test";
 import arrowDown from "../../../assets/arrow-down.svg";
 import arrowUp from "../../../assets/arrow-up.svg";
 
 export const FaqSection = () => {
-  const [clicked, setCLicked] = useState(false);
-  const handleClick = () => {
-    setCLicked(!clicked);
+  const [clicked, setClicked] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleClick = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
   };
+  const filteredFrequent = frequent.filter((elem) =>
+    elem.title.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
-    <section className="flex flex-col gap-[6px] justify-center items-center gradient-bg">
+    <section className="flex flex-col gap-[6px] justify-center items-center gradient-bg pb-[15%] ">
       <h3 className="text-[1.75rem]  text-upsel-white font-bold text-center mt-[15%] md:text-[2rem] lg:hidden  ">
         Have any questions?
       </h3>
       <h3 className="hidden text-[3.5rem]  text-upsel-white font-bold text-center mt-[15%]  lg:block ">
         Frequently asked questions
       </h3>
-      <div className="flex items-center justify-between bg-upsel-white rounded-[8px] w-3/5 my-[33px] p-[10px]">
+      <div className="flex items-center justify-between bg-upsel-white rounded-[8px] w-[90%]  my-[33px] p-[10px] md:w-4/5 lg:mb-[7%] lg:w-3/5">
         <div className="flex flex-col justify-center gap-[6px] w-3/4 ">
           <label
             className="text-[10px] italic text-upsel-light-grey-"
@@ -26,40 +35,57 @@ export const FaqSection = () => {
             Any Questions?
           </label>
           <input
-            className="text-[14px] w-full placeholder:text-upsel-black placeholder:italic"
-            type="search"
+            className="text-[14px] w-full outline-0 placeholder:text-upsel-black placeholder:italic"
             placeholder="Find answers to a question"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
           />
         </div>
-        <span className="">
-          <img src={search} alt="" />
+        <span className="hidden lg:block cursor-pointer">
+          <img src={search} alt="search icon" />
         </span>
       </div>
       <div className="flex flex-col gap-[6px] justify-center items-center w-full">
-        <div className="main p-[12px] rounded-lg bg-upsel-white w-4/5 md:w-7/8 ">
-          <div className="each px-[16px] w-full">
-            <div className="flex justify-between py-[16px]">
-              <h2 className="text-[18px] font-medium">
-                What web development services do you offer?
-              </h2>
-              <span onClick={handleClick} className="cursor-pointer">
-                {clicked ? (
-                  <img src={arrowUp} alt="" />
+        <div className="main flex flex-col gap-[6px] justify-center items-center p-[12px] rounded-lg bg-upsel-white w-[90%]  md:w-7/8 lg:w-4/5">
+          {filteredFrequent.map((elem, index) => {
+            return (
+              <div key={index} className="each py-[10px]  px-[16px] w-full">
+                <div className="flex justify-between lg:py-[16px]">
+                  <h2 className="text-[12px] font-medium lg:text-[18px]">
+                    {elem.title}
+                  </h2>
+                  <span
+                    onClick={() => handleClick(index)}
+                    className="cursor-pointer w-[24px] "
+                  >
+                    {clicked === index ? (
+                      <img src={arrowUp} alt="open accordion" />
+                    ) : (
+                      <img src={arrowDown} alt="closed accordion" />
+                    )}
+                  </span>
+                </div>
+                {clicked === index ? (
+                  <p className="text-[12px] mb-[10px] pt-[10px] border-t border-upsel-grey lg:text-[18px]">
+                    {elem.contentOne}
+                  </p>
                 ) : (
-                  <img src={arrowDown} alt="" />
+                  ""
                 )}
-              </span>
+              </div>
+            );
+          })}
+          <div className="flex flex-col items-center justify-center w-[279px] border-t border-upsel-grey mt-[18px] pt-[18px] lg:w-[368px] ">
+            <h5 className="text-[.875rem] font-bold ">Still have questions?</h5>
+            <p className="text-[.75rem] text-center">
+              If you cannot find answers to your question in our FAQ, you can
+              always contact us. We will answer to you shortly!
+            </p>
+            <div className="lg:mt-[2.94rem] md:mt-[1.5rem] mt-[1.89rem]">
+              <button className="text-[.875rem] italic text-upsel-white font-normal mb-[2rem] md:text-[1rem] lg:text-[1.25rem] rounded-[0.25rem] lg:w-[16.25rem] lg:px-[2rem] lg:py-[1rem] flex justify-center items-center bg-upsel-light-purple cursor-pointer md:w-[13.75rem] md:px-[1.5rem] md:py-[.75rem] w-[7.5rem] px-[1rem] py-[.5rem] transition ease-in-out delay-150 hover:bg-upsel-purple">
+                Contact Us
+              </button>
             </div>
-            {clicked && (
-              <p className="text-[18px] mb-[10px] pt-[10px] border-t border-upsel-grey">
-                We work with a variety of platforms and technologies, including
-                WordPress, Shopify, Magento, WooCommerce, React, Angular,
-                Node.js, Laravel, and more. Our team of experienced developers
-                stays up to date with the latest technologies and tools to
-                ensure that we can provide our clients with cutting-edge
-                solutions.
-              </p>
-            )}
           </div>
         </div>
       </div>
