1ee01f402b095b03f20e306370b88757b92b9ad9
ovara1mark <markovara1@gmail.com>
2023-09-23 21:51:04 +0100

    update

--- src/component/page/HomePage/index.jsx
+++ src/component/page/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Choose, HeroSection, More, Navbar } from "../../molecule";
 import {
   Footer,
@@ -7,20 +7,38 @@ import {
   FaqSection,
   DetailSection,
 } from "../../organism";
+import ReactLoading from "react-loading";
+import purple from "../../../assets/purple-3.gif";
 
 export const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading by using a timeout
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Set the duration as needed
+  }, []);
   return (
     <>
       <main className="bg-upsel-black">
-        <Navbar />
-        <HeroSection />
-        <AboutSection />
-        <More />
-        <DetailSection />
-        <Choose />
-        <FaqSection />
-        <ContactUs />
-        <Footer />
+        {isLoading ? (
+          <section className="bg-upsel-black w-[100%] h-[100vh] flex justify-center items-center">
+            <img src={purple} alt="preloader" />
+          </section>
+        ) : (
+          <>
+            <Navbar />
+            <HeroSection />
+            <AboutSection />
+            <More />
+            <DetailSection />
+            <Choose />
+            <FaqSection />
+            <ContactUs />
+            <Footer />
+          </>
+        )}
       </main>
     </>
   );
