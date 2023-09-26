e2d8a25334fccd52350f91d1c528a94089de0cfd
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2023-09-26 12:40:35 +0100

    Navbar complete

--- src/component/molecule/Navbar/index.jsx
+++ src/component/molecule/Navbar/index.jsx
@@ -4,11 +4,16 @@ import { Link } from "react-scroll";
 import menu from "../../../assets/menu.svg";
 import logo from "../../../assets/logo.svg";
 import close from "../../../assets/close.png";
+
 export const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
   return (
     <>
-      <div className="h-[5.4rem]  lg:h-[7.9rem] bg-upsel-black text-upsel-white lg:px-[6.24rem] lg:pt-[3.125rem] lg:pb-[1.88rem] py-[1.25rem] px-[2.5rem] flex mx-auto justify-between font-normal">
+      <div
+        className={`h-[5.4rem]  lg:h-[7.9rem] bg-upsel-black text-upsel-white lg:px-[6.24rem] lg:pt-[3.125rem] lg:pb-[1.88rem] py-[1.25rem] px-[2.5rem] flex mx-auto justify-between align-center font-normal ${
+          navbar ? "sticky top-0 z-[1000000]" : ""
+        } `}
+      >
         <div className="lg:flex lg:justify-start lg:items-center w-[6.5rem] cursor-pointer">
           <Link
             activeClass="active"
@@ -96,26 +101,28 @@ export const Navbar = () => {
             onClick={() => setNavbar(!navbar)}
           >
             {navbar ? (
-              <img src={close} width={30} height={30} alt="close" />
+              // <img src={close} width={20} height={20} alt="close" />
+              <span className="text-[1.4rem]">&#10006;</span>
             ) : (
-              <img
-                src={menu}
-                alt="menu"
-                width={40}
-                height={40}
-                className="focus:border-none active:border-none"
-              />
+              <span className="text-[1.4rem]">&#9776;</span>
+              // <img
+              //   src={menu}
+              //   alt="menu"
+              //   width={30}
+              //   height={30}
+              //   className="focus:border-none active:border-none"
+              // />
             )}
           </div>
         </div>
       </div>
       <div
-        className={`flex-1 justify-self-center pb-3 mt-8 bg-upsel-black md:hidden transition-transform transform translate-y-[0%]  ease-in-out rounded-t-lg fixed  w-full z-[1000000] md:pb-0 md:mt-0 ${
+        className={`flex-1 justify-self-center pb-3  bg-upsel-black md:hidden transition-transform transform translate-y-[0%]  ease-in-out  fixed  w-full z-[1000000] md:pb-0 md:mt-0 ${
           navbar ? "p-12 block" : "hidden"
         }`}
       >
         <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-          <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
+          <li className="pb-2 text-xl text-white py-6 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
             <Link
               activeClass="active"
               to="aboutus"
@@ -129,7 +136,7 @@ export const Navbar = () => {
               About Us
             </Link>
           </li>
-          <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
+          <li className="pb-2 text-xl text-white py-6 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
             <Link
               activeClass="active"
               to="Service"
@@ -143,7 +150,7 @@ export const Navbar = () => {
               Services
             </Link>
           </li>
-          <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
+          <li className="pb-2 text-xl text-white py-6 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
             <Link
               activeClass="active"
               to="Projects"
@@ -157,7 +164,7 @@ export const Navbar = () => {
               Projects
             </Link>
           </li>
-          <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
+          <li className="pb-2 text-xl text-white py-6 px-6 text-center  border-b-2 md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
             <Link
               activeClass="active"
               to="faq"
@@ -170,6 +177,22 @@ export const Navbar = () => {
             >
               FAQ
             </Link>
+          </li>
+          <li className="pb-2 text-xl text-white py-6 px-6 text-center md:border-b-0  hover:bg-upsel-purple  border-upsel-purple md:hover:text-upsel-purple md:hover:bg-transparent">
+            <button className="block text-[1rem] w-full rounded-[0.25rem] bg-upsel-light-purple px-[1.5rem] py-[0.75rem] transition ease-in-out delay-150 hover:bg-upsel-purple">
+              <Link
+                activeClass="active"
+                to="contactus"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                className="navbanner-text"
+                onClick={() => setNavbar(!navbar)}
+              >
+                Contact Us
+              </Link>
+            </button>
           </li>
         </ul>
       </div>
